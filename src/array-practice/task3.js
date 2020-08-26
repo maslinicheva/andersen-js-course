e665dd7661269feeb42699d12bf15999f844f6d8
sasha <ag.c2013@yandex.ru>
2020-08-26 23:36:08 +0300

    Task3 is done

--- src/array-practice/task3.js
+++ src/array-practice/task3.js
@@ -11,3 +11,8 @@
  *
  * forEachRight([1, 2, 3, 4], val => console.log(val)); -> в консоль 4 3 2 1
  */
+
+ function forEachRight(array, func){
+     array.reverse().forEach(func)
+ }
+ forEachRight([1, 2, 3, 4], val => console.log(val));
