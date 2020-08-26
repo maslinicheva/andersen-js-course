fab15b83c3ca05b9e818a6215165185a5bb213bd
sasha <ag.c2013@yandex.ru>
2020-08-26 22:51:19 +0300

    Task1 is done

--- src/array-practice/task1.js
+++ src/array-practice/task1.js
@@ -14,3 +14,10 @@
  * console.log(any([0, 0, 1, 0])); -> true
  * console.log(any([0, 0, 0, 0])); -> false
  */
+ function any(array, func = Boolean){
+     return array.some(func)
+ }
+
+ console.log(any([0, 1, 2, 0], x => x >= 2));
+ console.log(any([0, 0, 1, 0]));
+ console.log(any([0, 0, 0, 0]));
